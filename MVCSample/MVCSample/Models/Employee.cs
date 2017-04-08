03afb162838586e335c44459c4750a01277288c0
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@hotmail.com>
2017-04-08 21:11:24 +0600

    1st Commit

---
+++ MVCSample/MVCSample/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCSample.Models
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        [Display(Name ="ID")]
+        [Key]
+        public int Employee_ID { get; set; }
+        [Display(Name ="Name")]
+        public string Employee_Name { get; set; }
+        [Display(Name = "Salary")]
+        public string Employee_Salary { get; set; }
+        [Display(Name = "Department")]
+        public int Employee_Department { get; set; }
+    }
+}
